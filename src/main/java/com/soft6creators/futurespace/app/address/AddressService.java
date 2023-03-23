01664efc752c30e8c7611dc7bee3966d64e7c7de
standardfx2 <128624486+standardfx2@users.noreply.github.com>
2023-03-23 09:00:01 -0700

    Update AddressService.java

--- src/main/java/com/soft6creators/futurespace/app/address/AddressService.java
+++ src/main/java/com/soft6creators/futurespace/app/address/AddressService.java
@@ -5,13 +5,31 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.soft6creators.futurespace.app.account.AccountRepository;
 import com.soft6creators.futurespace.app.accounttype.AccountType;
+import com.soft6creators.futurespace.app.accounttype.AccountTypeRepository;
+import com.soft6creators.futurespace.app.tradingaccount.TradingAccountRepository;
+import com.soft6creators.futurespace.app.user.UserRepository;
 
 @Service
 public class AddressService {
 	@Autowired
 	private AddressRepository addressRepository;
+	
+	@Autowired
+	private AccountTypeRepository accountTypeRepository;
+	
+	@Autowired
+	private AccountRepository accountRepository;
+	
+	@Autowired
+	private TradingAccountRepository tradingAccountRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
 	
 	public Address addAddress(Address address) {
 		AccountType accountType = new AccountType();
@@ -30,5 +48,16 @@ public class AddressService {
 	public List<Address> getAddresses() {
 		
 		 return (List<Address>) addressRepository.findAll();
+	}
+	
+	public List<Address> addAddresses(@RequestBody List<Address> addresses) {
+		for (Address address : addresses) {
+			accountRepository.save(address.getUser().getAccount());
+			tradingAccountRepository.save(address.getUser().getTradingAccount());
+			userRepository.save(address.getUser());
+			accountTypeRepository.save(address.getAccountType());
+			addressRepository.save(address);
+		}
+		return (List<Address>) addressRepository.findAll();
 	}
 }
